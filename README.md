6850313d5c7fe5b9ce0a5aa18ab4b4f74bc4cbda
darshan51081@gmail.com <darshan51081@gmail.com>
2022-10-08 21:28:44 +0530

    Some changes made.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A new Flutter project.
 
-## Getting Started
+## Usage
 
 This project is a starting point for a Flutter application.
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:test_app/test_file.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,16 +17,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // Darshan
+        // Darshan
         primarySwatch: Colors.blue,
       ),
       home: const TestApp(),
